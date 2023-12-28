36b80034ac184aecba548e1dc2daf4dfe89bd432
Al Rafi <alrafi4@gmail.com>
2023-12-28 12:02:29 +0600

    commiting

--- src/Components/Dashboard/SuperAdminDashboard/SuperAdminDashboardMain/SchoolDashboard.jsx
+++ src/Components/Dashboard/SuperAdminDashboard/SuperAdminDashboardMain/SchoolDashboard.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import SchoolDashboardTop from './SchoolDashboardTop';
+import SchoolDashboardMiddle from './SchoolDashboardMiddle';
+import SchoolDashboardBottom from './SchoolDashboardBottom';
 
 const SchoolDashboard = () => {
     return (
         <div>
             <SchoolDashboardTop/>
+            <SchoolDashboardMiddle/>
+            <SchoolDashboardBottom/>
         </div>
     );
 };
