e4eda6f753d1fdcb3a94e9873a148fae781c4ffc
Shajibul Alam Shihab <shihab77023@gmail.com>
2023-12-29 11:07:41 +0600

    update

--- src/Components/Dashboard/SuperAdminDashboard/SuperDashboardHome/SuperHome.jsx
+++ src/Components/Dashboard/SuperAdminDashboard/SuperDashboardHome/SuperHome.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
-import TopSection from './TopSection';
-import TrendingTasks from './TrendingTasks';
-import Tasks from './Tasks';
+import React, { useEffect, useState } from "react";
+import TopSection from "./TopSection";
+import TrendingTasks from "./TrendingTasks";
+import Tasks from "./Tasks";
+import axios from "axios";
 
 const SuperHome = () => {
-    return (
-        <div>
-            <TopSection/>
-            <TrendingTasks/>
-            <Tasks></Tasks>
-        </div>
-    );
+  const [allTasks, setAllTasks] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_APP_SERVER_API}/api/v1/tasks`)
+      .then((tasks) => {
+        setAllTasks(tasks?.data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+  return (
+    <div>
+      <TopSection />
+      <TrendingTasks />
+      <Tasks allTasks={allTasks} />
+    </div>
+  );
 };
 
 export default SuperHome;
